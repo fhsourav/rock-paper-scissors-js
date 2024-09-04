e017162d937775fb1ad5a3f9a45339c92695d3c5
Fahim <69208613+fhsourav@users.noreply.github.com>
2024-09-05 02:41:05 +0600

    Created an UI for the game.

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
+const body = document.querySelector("body");
+
+const button_r = document.createElement("button");
+button_r.classList.add("btn");
+button_r.id = "rock";
+button_r.textContent = "rock";
+
+const button_p = document.createElement("button");
+button_p.classList.add("btn");
+button_p.id = "paper";
+button_p.textContent = "paper";
+
+const button_s = document.createElement("button");
+button_s.classList.add("btn");
+button_s.id = "scissors";
+button_s.textContent = "scissors";
+
+const div = document.createElement("div");
+div.classList.add("result");
+
+body.appendChild(button_r);
+body.appendChild(button_p);
+body.appendChild(button_s);
+body.appendChild(div);
+
+const p1 = document.createElement("p");
+const p2 = document.createElement("p");
+const h2 = document.createElement("h2");
+
+div.appendChild(p1);
+div.appendChild(p2);
+div.appendChild(h2);
+
+const buttons = document.querySelectorAll(".btn");
+
 let humanScore = 0;
 let computerScore = 0;
-// console.log("Hello World");
+let gameOver = false;
+
 function getComputerChoice() {
 	let random = Math.random()
 	if (random < 0.33) {
@@ -13,12 +49,12 @@ function getComputerChoice() {
 	
 }
 
-function getHumanChoice() {
-	let choice = prompt("rock paper scissors");
-	return choice;
-}
-
 function playRound(humanChoice, computerChoice) {
+
+	if (gameOver) {
+		return;
+	}
+	
 	humanChoice = humanChoice.toLowerCase();
 	if (humanChoice != computerChoice) {
 		if (humanChoice == "rock" && computerChoice == "scissors") {
@@ -31,19 +67,22 @@ function playRound(humanChoice, computerChoice) {
 			computerScore += 1;
 		}
 	}
-	console.log(humanScore);
-	console.log(computerScore);
+	p1.textContent = "Human choice: " + humanChoice + " | Computer choice: " + computerChoice;
+	p2.textContent = "Result: Human " + humanScore + " - " + computerScore + " Computer";
+	
+	if (humanScore == 5) {
+		gameOver = true;
+		h2.textContent = "Winner is human";
+	} else if (computerScore == 5) {
+		gameOver = true;
+		h2.textContent = "Winner is computer";
+	}
 }
 
-for (let index = 0; index < 5; index++) {
-	const humanSelection = getHumanChoice();
-	const computerSelection = getComputerChoice();
-	playRound(humanSelection, computerSelection);
-}
-if (humanScore > computerScore) {
-	console.log("Winner is human");
-} else if (humanScore < computerScore) {
-	console.log("Winner is computer");
-} else {
-	console.log("Draw");
-}
+buttons.forEach((button) => {
+	button.addEventListener("click", () => {
+		let computerSelection = getComputerChoice();
+		let humanSelection = button.id;
+		playRound(humanSelection, computerSelection);
+	})
+});
